6bcacda2f44a7edebd6829a5f9d40f54a153dac0
Rene Zaal <renezaal@gmail.com>
2015-07-03 22:25:35 +0200

    Initial programming, minor testing done

---
+++ AutoPCSX2SaveState/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AutoPCSX2SaveState
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.nudSaveInterval = new System.Windows.Forms.NumericUpDown();
+            this.nudButtonDelay = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHead = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSaveInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudButtonDelay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnStartStop.Location = new System.Drawing.Point(0, 73);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(271, 23);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // nudSaveInterval
+            // 
+            this.nudSaveInterval.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudSaveInterval.Location = new System.Drawing.Point(159, 21);
+            this.nudSaveInterval.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudSaveInterval.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudSaveInterval.Name = "nudSaveInterval";
+            this.nudSaveInterval.Size = new System.Drawing.Size(91, 20);
+            this.nudSaveInterval.TabIndex = 1;
+            this.nudSaveInterval.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // nudButtonDelay
+            // 
+            this.nudButtonDelay.Location = new System.Drawing.Point(159, 47);
+            this.nudButtonDelay.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudButtonDelay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudButtonDelay.Name = "nudButtonDelay";
+            this.nudButtonDelay.Size = new System.Drawing.Size(91, 20);
+            this.nudButtonDelay.TabIndex = 2;
+            this.nudButtonDelay.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time between saves";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Delay after button press";
+            // 
+            // lblHead
+            // 
+            this.lblHead.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHead.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblHead.Location = new System.Drawing.Point(0, 0);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(271, 15);
+            this.lblHead.TabIndex = 5;
+            this.lblHead.Text = "All times are expressed in seconds";
+            this.lblHead.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 96);
+            this.Controls.Add(this.lblHead);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudButtonDelay);
+            this.Controls.Add(this.nudSaveInterval);
+            this.Controls.Add(this.btnStartStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Automatic state saver";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSaveInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudButtonDelay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.NumericUpDown nudSaveInterval;
+        private System.Windows.Forms.NumericUpDown nudButtonDelay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHead;
+    }
+}
+
---
+++ AutoPCSX2SaveState/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoPCSX2SaveState
+{
+    public partial class Form1 : Form
+    {
+        private bool running = false;
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        private System.Timers.Timer delayTimer = new System.Timers.Timer();
+        private int timesSaved = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Elapsed += timer_Elapsed;
+            delayTimer.Elapsed += timer_Elapsed;
+            delayTimer.AutoReset = false;
+            timer.AutoReset = true;
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timerExpired();
+        }
+
+        private void start()
+        {
+            timer.Interval = (double)nudSaveInterval.Value * 1000;
+            timer.Start();
+            btnStartStop.Text = "Stop";
+            this.Text = "AutoSaveState - Running";
+        }
+
+        private void stop()
+        {
+            timer.Stop();
+            delayTimer.Stop();
+            btnStartStop.Text = "Start";
+            this.Text = "AutoSaveState - Stopped";
+        }
+
+        delegate void timerExpiredDelegate();
+        private void timerExpired()
+        {
+            if (lblHead.InvokeRequired)
+            {
+                lblHead.Invoke(new timerExpiredDelegate(timerExpired));
+                return;
+            }
+            double idleTime = getIdleTimeInSeconds();
+            double minIdleTime = (double)nudButtonDelay.Value;
+            lblHead.Text = String.Format("Idle time: {0}, times saved: {1}", idleTime, timesSaved);
+
+            // if the user has not pressed a button for a time longer than the minimum idle time we send the keystrokes
+            if (idleTime + 0.001 > minIdleTime)
+            {
+                // stop the delaytimer since it is not needed anymore
+                delayTimer.Stop();
+                // send the keystrokes
+                sendKeystrokes();
+                // quit the function since we did all we needed to do
+                return;
+            }
+
+            // this method should fire again as soon as the user is projected to have been idle long enough 
+            delayTimer.Interval = (minIdleTime - idleTime) * 1000;
+            // start the timer
+            delayTimer.Start();
+        }
+
+        private void sendKeystrokes()
+        {
+            SendKeys.SendWait("{F2}");
+            System.Threading.Thread.Sleep(50);
+            SendKeys.SendWait("{F1}");
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (running)
+            { stop(); }
+            else
+            { start(); }
+            running = !running;
+        }
+
+        private double getIdleTimeInSeconds()
+        {
+            // somehow this returns in seconds
+            return TimeSpan.FromTicks(IdleTimeFinder.GetIdleTime()).TotalMilliseconds;
+        }
+    }
+
+    internal struct LASTINPUTINFO
+    {
+        public uint cbSize;
+
+        public uint dwTime;
+    }
+
+    /// <summary>
+    /// Helps to find the idle time, (in ticks) spent since the last user input
+    /// </summary>
+    public class IdleTimeFinder
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+        /// <summary>
+        /// Get the Last input time in ticks
+        /// </summary>
+        /// <returns></returns>
+        public static long GetLastInputTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            if (!GetLastInputInfo(ref lastInPut))
+            {
+                throw new Exception(GetLastError().ToString());
+            }
+            return lastInPut.dwTime;
+        }
+    }
+}
